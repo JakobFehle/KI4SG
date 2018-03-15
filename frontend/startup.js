acdee826e8d0f4c4b482c7747b5798425c9c1c59
David Halbhuber <david.halbhuber@stud.uni-regensburg.de>
2018-03-15 02:15:50 +0100

    Node.JS
    Added Node, Server:9009

---
+++ frontend/startup.js
@@ -0,0 +1,11 @@
+//Initiate Server Instance
+//Listing on Port 9009
+//Use startServer.bat for instantiation
+//Or use "node startup.js" in CMD
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('FRONTEND');
+}).listen(9009);
