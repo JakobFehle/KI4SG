9438f86b303539d3e74b104e1a07f8323a339b88
David Halbhuber <david.halbhuber@stud.uni-regensburg.de>
2018-03-15 03:12:51 +0100

    landing

--- frontend/startup.js
+++ frontend/startup.js
@@ -3,10 +3,15 @@
 //Use startServer.bat for instantiation
 //Or use "node startup.js" in CMD
 
-var http = require('http');
-var db = require('./databaseConnector')
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('FRONTEND');
-}).listen(9009);
+var db = require('./databaseConnector')
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('FRONTEND!');
+});
+
+app.listen(9009, function () {
+  console.log('Server startet @localhost:9009');
+});
