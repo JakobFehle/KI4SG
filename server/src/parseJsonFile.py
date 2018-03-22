c739c3930cbad87ae9d30e0a9f0050aa5ea91571
David Halbhuber <david.halbhuber@stud.uni-regensburg.de>
2018-03-22 15:55:19 +0100

    added error handling for parser

--- server/src/parseJsonFile.py
+++ server/src/parseJsonFile.py
@@ -15,12 +15,16 @@ class CompileJsons:
         self.__calcualteNutr()
         
     def __calcualteNutr(self):
-        with io.open(r"../../json/testparse.json", encoding="utf-8") as json_file:
+        with io.open(r"../../json/final_recipe_180001to210000.json", encoding="utf-8") as json_file:
             self.jsonFile = json.load(json_file)
             for jsonData in self.jsonFile:
-                rezept = Rezept(jsonData)
-                nutr = rezept.returnNutrArray()
+                try:
+                    rezept = Rezept(jsonData)
+                    nutr = rezept.returnNutrArray()
 
-                writeNuts(nutr[0],nutr[1],nutr[2],nutr[3],nutr[4],nutr[5],nutr[6],nutr[7],nutr[8],nutr[9],nutr[10],nutr[11],nutr[12],nutr[13],nutr[14],nutr[15],nutr[16],nutr[17],nutr[18],nutr[19],nutr[20])
-           
+                    writeNuts(nutr[0],nutr[1],nutr[2],nutr[3],nutr[4],nutr[5],nutr[6],nutr[7],nutr[8],nutr[9],nutr[10],nutr[11],nutr[12],nutr[13],nutr[14],nutr[15],nutr[16],nutr[17],nutr[18],nutr[19],nutr[20])
+                    print nutr[0]
+                except:
+                    print "Failure parsing Entry"
+
 compileJsons=CompileJsons()
